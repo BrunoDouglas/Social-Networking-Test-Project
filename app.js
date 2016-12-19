93e844783df06d2b4897a48ad1a80a36fae043ac
BrunoDouglas <brunodouglas@gmail.com>
2016-12-19 16:20:40 -0600

    Updated the file content, it was missing the rest of the statements

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-var http =x
+var http = require("http");
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type":"text/plain"});
+  response.write("Hello World!");
+  response.end();
+}).listen(3000);
